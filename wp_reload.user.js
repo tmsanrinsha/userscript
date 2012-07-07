a70281f51336a8e0d1c1a4837e9ec1dcc266bde9
Takashi Miwa <sanrinsha.tm@gmail.com>
2012-07-07 23:55:22 +0900

    wp_reload.user.jsに説明を加えた

--- wp_reload.user.js
+++ wp_reload.user.js
@@ -8,8 +8,12 @@
 (function() {
     var ta = document.getElementById("content");
     ta.addEventListener("change", function() {
+        // It's All Text!によってtextareaのchangeイベントが発火したのかどうかをtextareaのbackground-colorで判定
+        // background-colorの変化にタイムラグがあるため100ミリ秒待つ
         setTimeout(function() {
             if(ta.style.backgroundColor) {
+                // このclickはIE,Firefoxでしか使えないので注意
+                // http://yakinikunotare.boo.jp/orebase2/javascript/fire_click_event
                 document.getElementById('post-preview').click();
             }
         }, 100);
